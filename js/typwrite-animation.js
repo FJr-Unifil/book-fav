ef8f789e0fed79990b7af5f1975a0407409c3647
FernandoJunior7 <fernando.junior@edu.unifil.br>
2024-08-19 10:35:46 -0300

    Implement typerite animation to homepage

---
+++ js/typwrite-animation.js
@@ -0,0 +1,43 @@
+const phrases = [
+  'Isso é BookFav',
+  'Crie lista de livros',
+  'Offline e Fácil',
+];
+let currentPhraseIndex = 0;
+let currentCharIndex = 0;
+let isTyping = true;
+const typingSpeed = 100;
+const pauseDuration = 500;
+
+const typingElement = document.getElementById('typewriter');
+
+function typeText() {
+  if (isTyping) {
+    if (currentCharIndex < phrases[currentPhraseIndex].length) {
+      typingElement.textContent = phrases[currentPhraseIndex].slice(
+        0,
+        currentCharIndex + 1
+      );
+      currentCharIndex++;
+      setTimeout(typeText, typingSpeed);
+    } else {
+      isTyping = false;
+      setTimeout(typeText, pauseDuration);
+    }
+  } else {
+    if (currentCharIndex > 0) {
+      typingElement.textContent = phrases[currentPhraseIndex].slice(
+        0,
+        currentCharIndex - 1
+      );
+      currentCharIndex--;
+      setTimeout(typeText, typingSpeed);
+    } else {
+      isTyping = true;
+      currentPhraseIndex = (currentPhraseIndex + 1) % phrases.length;
+      setTimeout(typeText, pauseDuration);
+    }
+  }
+}
+
+typeText();
