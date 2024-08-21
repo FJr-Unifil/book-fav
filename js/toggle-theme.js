55c3277afd1f809043ee3451d13b2b45f3f86733
FernandoJunior7 <fernando.junior@edu.unifil.br>
2024-08-21 18:53:17 -0300

    Theme state saved on the localStorage

--- js/toggle-theme.js
+++ js/toggle-theme.js
@@ -1,7 +1,23 @@
 const root = document.documentElement;
 const toggleThemeBtn = document.querySelector('#theme-toggle');
 
+const DARK_THEME = 'dark';
+const LIGHT_THEME = 'light';
+const THEME_KEY = 'bf-theme';
+
+function loadTheme() {
+  root.className = localStorage.getItem(THEME_KEY) || LIGHT_THEME;
+}
+
+function updateThemeOnLocalStorage() {
+  localStorage.getItem(THEME_KEY) == LIGHT_THEME
+    ? localStorage.setItem(THEME_KEY, DARK_THEME)
+    : localStorage.setItem(THEME_KEY, LIGHT_THEME);
+}
+
 toggleThemeBtn.addEventListener('click', () => {
-  root.classList.toggle('light');
-  root.classList.toggle('dark');
+  updateThemeOnLocalStorage();
+  loadTheme();
 });
+
+loadTheme();
